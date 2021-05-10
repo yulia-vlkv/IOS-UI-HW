db1fa33851f6c5926703e315eecfeb3bfd2f5f1a
Iuliia Vlkv <yu.wlkw@gmail.com>
2021-05-10 21:20:58 +0500

    Finally!!

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Iuliia Volkova on 10.05.2021.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    @IBOutlet weak var profilePicture: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var birthdayDate: UILabel!
+    @IBOutlet weak var city: UILabel!
+    @IBOutlet weak var someRandomText: UITextView!
+    
+}
